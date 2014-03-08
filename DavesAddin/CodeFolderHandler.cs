ab009dfa6f23b1969a284d31a84db09774cd2fdb
Dave <newky2k@mac.com>
2014-03-08 22:21:32 +0000

    Initial work on adding
    Working code collapse
    
    Initial version changer menu

---
+++ DavesAddin/CodeFolderHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Ide;
+using Mono.TextEditor;
+using MonoDevelop.Ide.Gui.Content;
+
+namespace DavesAddin
+{
+	public enum CodeFoldingCommands
+	{
+		CollapseToDefs,
+	}
+
+	public class CodeFoldingHandler : CommandHandler
+	{
+		protected override void Run ()
+		{
+			//var item = IdeApp.
+			var item = IdeApp.Workbench.ActiveDocument;
+
+			if (item != null)
+			{
+				var editor = item.ActiveView.GetContent <IFoldable> () as MonoDevelop.SourceEditor.SourceEditorView;
+				var Document = item.Editor.Document;
+
+				foreach (FoldSegment segment in Document.FoldSegments)
+				{
+					if (segment.FoldingType == FoldingType.TypeDefinition)
+					{
+						segment.IsFolded = true;
+					}
+					if (segment.FoldingType == FoldingType.TypeMember || segment.FoldingType == FoldingType.Comment)
+						segment.IsFolded = true;
+				}
+
+				editor.TextEditor.Caret.MoveCaretBeforeFoldings ();
+				Document.RequestUpdate (new UpdateAll ());
+				Document.CommitDocumentUpdate ();
+				editor.TextEditor.GetTextEditorData ().RaiseUpdateAdjustmentsRequested ();
+				editor.TextEditor.ScrollToCaret ();
+			}
+
+		}
+
+		protected override void Update (CommandInfo info)
+		{  
+			var doc = IdeApp.Workbench.ActiveDocument; 
+			info.Enabled = doc != null && doc.GetContent<ITextEditorDataProvider> () != null; 
+		}
+	}
+}
+
---
+++ DavesAddin/VersionChanger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Ide;
+using Mono.TextEditor;
+
+namespace DavesAddin
+{
+	public enum VersionChangerCommands
+	{
+		UpdateVersions,
+	}
+
+	public class VersionChangerHandler : CommandHandler
+	{
+		protected override void Run ()
+		{
+			//var item = IdeApp.
+			var item = IdeApp.ProjectOperations.CurrentSelectedSolution;
+
+			if (item != null)
+			{
+				//
+			}
+
+		}
+
+		protected override void Update (CommandInfo info)
+		{  
+			info.Enabled = true; 
+		}
+	}
+}
+
